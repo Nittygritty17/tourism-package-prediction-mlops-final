83eee87f35db6f293e47321b960c929499ea5a4c
Nittygritty17 <akansharaj.off@gmail.com>
2026-04-01 00:36:08 +0530

    Clean Gender data and remove unnecessary columns
    Replace 'Fe Male' with 'Female' in Gender column and drop specified columns if they exist.

--- train.py
+++ train.py
@@ -1,1 +1,9 @@
-# placeholder
+for dftemp in [traindf, testdf]:
+    if "Gender" in dftemp.columns:
+        dftemp["Gender"] = dftemp["Gender"].replace("Fe Male", "Female")
+
+    cols_to_drop = ["Unnamed: 0", "CustomerID", "__index_level_0__"]
+    existing_cols = [col for col in cols_to_drop if col in dftemp.columns]
+
+    if existing_cols:
+        dftemp.drop(columns=existing_cols, inplace=True)
